7a601961d4cf11b58a0b1f6007204ec0a5f3cfe6
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-07-02 07:03:10 +0300

    Rejuvenating my moods

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
+import { func } from "prop-types";
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>This is a new trail in react</div>;
+  const [joke, setJoke] = useState([]);
+  const [num, setNum] = useState(0);
+
+  async function getPunchlines() {
+    const res = await fetch(
+      "https://official-joke-api.appspot.com/jokes/random"
+    );
+    const data = await res.json();
+
+    setJoke(data);
+    setNum((n) => n + 1);
+  }
+
+  useEffect(function () {
+    getPunchlines();
+  }, []);
+
+  return (
+    <div className="app">
+      <div>Type: {joke.type}</div>
+      <div>Setup: {joke.setup}</div>
+      <div>Punchline: {joke.punchline}</div>
+      <button onClick={getPunchlines}>Click me </button>
+      <Message num={num} />
+    </div>
+  );
+}
+
+function Message({ num }) {
+  return (
+    <p>
+      You have read <b>{num}</b> punchlines today
+    </p>
+  );
 }
